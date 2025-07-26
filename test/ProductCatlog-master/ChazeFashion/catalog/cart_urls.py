467e08779f915330482a67e7806503657cc06069
Harshx02 <tiwariharsh2204@gmail.com>
2025-07-26 14:06:51 +0530

    fixed4

--- test/ProductCatlog-master/ChazeFashion/catalog/cart_urls.py
+++ test/ProductCatlog-master/ChazeFashion/catalog/cart_urls.py
@@ -3,10 +3,16 @@ from catalog.views import (
     add_to_cart,
     cart,
     remove_from_cart,
+    update_cart_item,
+    move_to_wishlist,
 )
 
 urlpatterns = [
     path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
- path('', cart, name='view_cart'),  # this replaces view_cart
+    path('', cart, name='view_cart'),  # this replaces view_cart
     path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('update/<int:product_id>/', update_cart_item, name='update_cart_item'),
+    path('move-to-wishlist/<int:item_id>/', move_to_wishlist, name='move_to_wishlist'),
 ]
+
+
